38b86244dad726435952f27a602a06a985d01aa0
José Enrique Díaz García <tucorreo@email.com>
2026-03-10 22:16:22 -0600

    AS1

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 $host = getenv('DB_HOST') ?: 'localhost';
 $db   = getenv('DB_NAME') ?: 'contratosdiseno';
 $user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASSWORD') ?: '121318';
+$pass = getenv('DB_PASSWORD') ?: '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
